afd2bf659b4a2ea568cba28aa3ebf73017d45c19
Ryan Rogers <ryan@timewasted.me>
2014-03-27 16:55:48 -0700

    Update cipher suite ordering.

--- server.go
+++ server.go
@@ -22,17 +22,23 @@ import (
 // because they are not (yet?) supported by Go. Defining them here allows us
 // to immediately start using them, should Go support them in the future.
 const (
+	TLS_DHE_RSA_WITH_3DES_EDE_CBC_SHA       uint16 = 0x0016
 	TLS_DHE_RSA_WITH_AES_128_CBC_SHA        uint16 = 0x0033
 	TLS_DHE_RSA_WITH_AES_256_CBC_SHA        uint16 = 0x0039
+	TLS_RSA_WITH_AES_128_CBC_SHA256         uint16 = 0x003c
+	TLS_RSA_WITH_AES_256_CBC_SHA256         uint16 = 0x003d
+	TLS_DHE_RSA_WITH_AES_128_CBC_SHA256     uint16 = 0x0067
+	TLS_DHE_RSA_WITH_AES_256_CBC_SHA256     uint16 = 0x006b
 	TLS_RSA_WITH_AES_128_GCM_SHA256         uint16 = 0x009c
 	TLS_RSA_WITH_AES_256_GCM_SHA384         uint16 = 0x009d
 	TLS_DHE_RSA_WITH_AES_128_GCM_SHA256     uint16 = 0x009e
 	TLS_DHE_RSA_WITH_AES_256_GCM_SHA384     uint16 = 0x009f
-	TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA    uint16 = 0xc009
-	TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA    uint16 = 0xc00a
-	TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256 uint16 = 0xc02b
+	TLS_ECDHE_ECDSA_WITH_3DES_EDE_CBC_SHA   uint16 = 0xc008
+	TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256 uint16 = 0xc023
+	TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384 uint16 = 0xc024
+	TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256   uint16 = 0xc027
+	TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384   uint16 = 0xc028
 	TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384 uint16 = 0xc02c
-	TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256   uint16 = 0xc02f
 	TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384   uint16 = 0xc030
 )
 
@@ -119,42 +125,51 @@ func (s *Server) initialTLSConfiguration() *tls.Config {
 		NextProtos:   []string{"http/1.1"},
 		// Reasoning behind the cipher suite ordering:
 		//
-		// - We want forward secrecy, so ECDHE/DHE come first. ECDHE comes
-		//   before DHE since it's both stronger and faster.
-		// - We prefer ECDSA over RSA since it's both stronger and faster.
-		// - AES-GCM is currently our best choice of ciphers, since it is not
-		//   vulnerable to any known attacks.
-		// - Between CBC-mode ciphers and RC4, I'm not sure which is the lesser
-		//   evil. CBC is vulnerable to BEAST (which is mostly mitigated by
+		// - Forward secrecy is first priority. ECDHE beats DHE on strength
+		//   and speed, so try it first.
+		// - ECDSA beats RSA on strength and speed, so try it first.
+		// - AES-GCM is not vulnerable to any known attacks, so use it if at
+		//   all possible.
+		// - CBC-mode ciphers are a lesser evil compared to the other
+		//   alternatives. It is vulnerable to BEAST (which is mitigated by
 		//   modern clients: https://community.qualys.com/blogs/securitylabs/2013/09/10/is-beast-still-a-threat)
-		//   and Lucky13 (which is unlikely to be mitigated in Go: https://groups.google.com/d/msg/golang-nuts/HF5O5vAKRcQ/3cYWryRyZboJ),
-		//   while RC4 has its own set of issues which lead to questionable
-		//   security. For now, I'm opting to prefer RC4 just because that
-		//   seems to be the consensus among the internet giants that employ
-		//   people who are undoubtedly much smarter than me about this sort
-		//   of thing.
+		//   and Lucky13 (which is unlikely to be mitigated in Go: https://groups.google.com/d/msg/golang-nuts/HF5O5vAKRcQ/3cYWryRyZboJ).
+		//   The alternative is RC4, which is pretty well broken these days.
+		// - Fall back to 3DES as a last resort.
 		CipherSuites: []uint16{
 			TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-			TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-			TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
-			TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
-
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 			TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_RSA_WITH_RC4_128_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+
+			TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384,
+			TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,
+			TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384,
+			TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,
+
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
 			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
 			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
 
 			TLS_DHE_RSA_WITH_AES_256_GCM_SHA384,
 			TLS_DHE_RSA_WITH_AES_128_GCM_SHA256,
+			TLS_DHE_RSA_WITH_AES_256_CBC_SHA256,
+			TLS_DHE_RSA_WITH_AES_128_CBC_SHA256,
 			TLS_DHE_RSA_WITH_AES_256_CBC_SHA,
 			TLS_DHE_RSA_WITH_AES_128_CBC_SHA,
 
 			TLS_RSA_WITH_AES_256_GCM_SHA384,
 			TLS_RSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_RSA_WITH_RC4_128_SHA,
+			TLS_RSA_WITH_AES_256_CBC_SHA256,
+			TLS_RSA_WITH_AES_128_CBC_SHA256,
 			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
 			tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+
+			TLS_ECDHE_ECDSA_WITH_3DES_EDE_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA,
+			TLS_DHE_RSA_WITH_3DES_EDE_CBC_SHA,
+			tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA,
 		},
 		PreferServerCipherSuites: true,  // Prefer our strong ciphers
 		SessionTicketsDisabled:   false, // Support session tickets
